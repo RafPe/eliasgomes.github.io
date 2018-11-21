dd5ea9a71ccc7ce06b2c3357803cfeabcdd01eb2
Elias Gomes <eliasgomes@gmail.com>
2018-11-21 16:05:18 +0100

    Initial commit

---
+++ precache-manifest.520cf3e76e636895bac159a183b85656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d86a609d800e2cbc47b7",
+    "url": "/static/js/main.d86a609d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "d86a609d800e2cbc47b7",
+    "url": "/static/css/main.ca8bed0a.chunk.css"
+  },
+  {
+    "revision": "33dc9198cf9a3ee94ca6150b1a46a025",
+    "url": "/index.html"
+  }
+];
